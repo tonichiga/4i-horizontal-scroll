5663ef020b2fbd726f202bd800949e90110ae35d
Toni <tonichiga@gmail.com>
2023-02-18 14:31:09 +0200

    fix posiiton scroll container and currently calculate scroll end coords

--- scroll.js
+++ scroll.js
@@ -11,15 +11,19 @@ class Scroll {
   observer = null;
   root = null;
   parentTarget = null;
+  scrollWrapper = null;
   onScrollEvent = null;
   direction = "left";
   pagePositionY = 0;
+  leftOffset = 0;
 
-  connect({ root, target, start = 0, onScrollEvent }) {
+  connect({ root, target, start = 0, onScrollEvent, scrollWrapper }) {
     this.start = start;
     this.target = target;
     this.root = root;
+    this.scrollWrapper = scrollWrapper;
     this.onScrollEvent = onScrollEvent;
+    this.leftOffset = this.calculateLeftOffsetContainer();
     this.setDefaultStyles();
     this.calculateOffsettarget();
     this.resizeInit();
@@ -31,49 +35,68 @@ class Scroll {
     parentNode.replaceChild(wrapper, this.target);
     wrapper.appendChild(this.target);
     this.parentTarget = wrapper;
+    const rootLeftOffset = this.leftOffset;
 
     this.root.style.position = "relative";
     this.root.style.width = "100%";
+    this.root.style.left = -rootLeftOffset + "px";
+
     this.target.style.transition = "transform 150ms ease-out";
     document.body.style.overflowX = "hidden";
 
     this.parentTarget.style.display = "block";
-    this.parentTarget.style.position = "sticky";
-    this.parentTarget.style.overflow = "hidden";
-    this.parentTarget.style.top = this.start;
-    this.parentTarget.style.minHeight = `70vh`;
+    // this.parentTarget.style.position = 'sticky';
+    // this.parentTarget.style.overflow = "hidden";
+    // this.parentTarget.style.top = this.start;
+    // this.parentTarget.style.minHeight = `70vh`;
+
+    if (this.scrollWrapper) {
+      this.scrollWrapper.style.top = this.start;
+      this.scrollWrapper.style.position = "sticky";
+    } else {
+      this.parentTarget.style.top = this.start;
+      this.parentTarget.style.position = "sticky";
+    }
 
     this.setRootHeight();
   }
 
   setRootHeight() {
     const height = this.target.clientWidth - window.innerWidth;
-    const percent = height * 0.05;
+    const percent = height * 0.1;
     this.root.style.height = `${height + percent}px`;
+  }
+
+  calculateLeftOffsetContainer() {
+    let rect = this.root.getBoundingClientRect();
+
+    return rect.left;
   }
 
   calculateOffsettarget() {
     let rect = this.target.getBoundingClientRect();
     this.top = rect.top + window.scrollY;
-    this.finishCoords = rect.width - window.innerHeight;
 
+    this.finishCoords = rect.width - window.innerHeight;
     this.onScroll();
   }
 
   handleScroll() {
     const lastKnownScrollPosition = window.scrollY - this.top;
-    let differenceOffset =
-      (this.root.clientWidth / this.target.clientWidth) * 100;
+    const targetWidth = this.root.clientWidth + this.leftOffset * 2;
+    let differenceOffset = (targetWidth / this.target.clientWidth) * 100;
     const offset = 100 - differenceOffset;
+
+    console.log("Diff", differenceOffset);
 
     if (lastKnownScrollPosition < 0) return;
 
-    if (this.pagePositionY > lastKnownScrollPosition) {
-      //direction
-      this.direction = "left";
-    } else {
-      this.direction = "right";
-    }
+    // if (this.pagePositionY > lastKnownScrollPosition) {
+    //   //direction
+    //   this.direction = 'left';
+    // } else {
+    //   this.direction = 'right';
+    // }
 
     this.pagePositionY = lastKnownScrollPosition;
 
@@ -88,7 +111,7 @@ class Scroll {
       progress = offset;
     }
 
-    if (Math.abs(progress) < 3) {
+    if (Math.abs(progress) < 1.5) {
       progress = 0;
     }
 
@@ -155,6 +178,16 @@ class Scroll {
 
   scrollAnimation(target, to, duration = 150) {
     target.style.transform = `translate3d(-${to}%, 0, 0)`;
+
+    // const newspaperSpinning = [{ transform: `translate3d(-${to}%, 0, 0)` }];
+
+    // const newspaperTiming = {
+    //   duration,
+    //   forwards: true,
+    //   iterations: 1,
+    // };
+
+    // target.animate(newspaperSpinning, newspaperTiming);
   }
 }
 
